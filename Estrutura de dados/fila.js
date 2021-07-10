d9d1ea7b4399da0caaff993fe005fa421bf476e4
IzaqueL95 <izaque_14@live.com>
2021-07-10 00:13:37 -0300

    commit com arquivos de estudo iniciais

---
+++ Estrutura de dados/fila.js
@@ -0,0 +1,14 @@
+var fila = [30, 20, 40, 80, 5, 14];
+
+function findNumPosition(num){
+    for(i=0; i<6; i++){
+        if(num == fila[i]){
+            return i;
+        } 
+    } return -1;
+}
+
+console.log(findNumPosition(40));
+console.log(findNumPosition(12));
+
+//CTRL ALT + N 
---
+++ JavaScript Basico/script.js
@@ -0,0 +1,15 @@
+var myName = 'Izaque'
+
+console.log(`My name is ${myName}`)
+
+let language = 'JavaScript'
+const country = 'Brazil'
+
+console.log(typeof true)
+
+/* Var pode ser usada em ESCOPO GLOBAL, ESCOPO DE FUNÇÃO, PODE SER RESSIGNIFICADO, PODE SER REDECLARADO, PODE SER HOSPEDADO*/
+
+/* lET não pode ser usada em ESCOPO GLOBAL, e não PODE SER REDECLARADO, e não PODE SER HOSPEDADO 
+  mas pode em ESCOPO DE FUNÇÃO, PODE SER RESSIGNIFICADO, */
+
+/*Const não pode ser usada em ESCOPO GLOBAL, não pode SER RESSIGNIFICADO não pode ser REDECLARADO e não pode ser HOSPEDADO*/
